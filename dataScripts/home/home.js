24830ddf17bebb72e473ac883e87f03a32262e24
SatsAjeet <98746450+SatsAjeet@users.noreply.github.com>
2022-04-01 10:21:14 +0530

    commit

---
+++ dataScripts/home/home.js
@@ -0,0 +1,307 @@
+
+let img=[
+    {
+        image:"	https://assets.pbimgs.com/pbimgs/rk/images/dp/ecm/202213/2220/001/001.jpg"
+    },
+    // {
+    // image1:"https://assets.pbimgs.com/pbimgs/rk/images/dp/ecm/202209/1386/001/032.jpg"
+    // }
+];
+
+//console.log(img);
+
+localStorage.setItem("av",JSON.stringify(img));
+let arv= JSON.parse(localStorage.getItem("av"));
+
+
+arv.map(function(ele){
+    let div=document.createElement("div");
+
+    let pic=document.createElement("img");
+
+    pic.src=ele.image;
+    div.append(pic);
+    document.querySelector("#arriveimg").append(div);
+})
+
+
+let img1=[
+    
+    {
+    image1:"https://assets.pbimgs.com/pbimgs/rk/images/dp/ecm/202209/1386/001/032.jpg"
+    }
+];
+
+//console.log(img);
+
+localStorage.setItem("av1",JSON.stringify(img1));
+
+
+let arv1= JSON.parse(localStorage.getItem("av1"));
+
+
+arv1.map(function(ele){
+    let div1=document.createElement("div");
+
+    let pic1=document.createElement("img");
+
+    pic1.src=ele.image1;
+    div1.append(pic1);
+    document.querySelector("#arriveimg1").append(div1);
+})
+
+
+let img2=[
+    
+    {
+    image2:"https://assets.pbimgs.com/pbimgs/rk/images/dp/ecm/202212/1568/001/003.jpg"
+    }
+];
+
+//console.log(img);
+
+localStorage.setItem("av2",JSON.stringify(img2));
+
+
+let arv2= JSON.parse(localStorage.getItem("av2"));
+
+
+arv2.map(function(ele){
+    let div2=document.createElement("div");
+
+    let pic2=document.createElement("img");
+
+    pic2.src=ele.image2;
+    div2.append(pic2);
+    document.querySelector("#arriveimg2").append(div2);
+})
+
+
+
+
+
+let img3=[
+    
+    {
+    image3:"https://assets.pbimgs.com/pbimgs/rk/images/dp/ecm/202209/1386/001/039.jpg"
+    }
+];
+
+//console.log(img);
+
+localStorage.setItem("av3",JSON.stringify(img3));
+
+
+let arv3= JSON.parse(localStorage.getItem("av3"));
+
+
+arv3.map(function(ele){
+    let div3=document.createElement("div");
+
+    let pic3=document.createElement("img");
+
+    pic3.src=ele.image3;
+    div3.append(pic3);
+    document.querySelector("#arriveimg3").append(div3);
+})
+
+
+let img4=[
+    
+    {
+    image4:"https://assets.pbimgs.com/pbimgs/rk/images/dp/ecm/202213/0277/001/002.jpg"
+    }
+];
+
+//console.log(img);
+
+localStorage.setItem("av4",JSON.stringify(img4));
+
+
+let arv4= JSON.parse(localStorage.getItem("av4"));
+
+
+arv4.map(function(ele){
+    let div4=document.createElement("div");
+
+    let pic4=document.createElement("img");
+
+    pic4.src=ele.image4;
+    div4.append(pic4);
+    document.querySelector("#arriveimg4").append(div4);
+})
+
+
+
+
+let img5=[
+    
+    {
+    image5:"https://assets.pbimgs.com/pbimgs/rk/images/dp/ecm/202213/0277/001/001.jpg"
+    }
+];
+
+//console.log(img);
+
+localStorage.setItem("av5",JSON.stringify(img5));
+
+
+let arv5= JSON.parse(localStorage.getItem("av5"));
+
+
+arv5.map(function(ele){
+    let div5=document.createElement("div");
+
+    let pic5=document.createElement("img");
+
+    pic5.src=ele.image5;
+    div5.append(pic5);
+    document.querySelector("#arriveimg5").append(div5);
+})
+
+
+let img6=[
+    
+    {
+    image6:"https://assets.pbimgs.com/pbimgs/rk/images/dp/ecm/202152/6257/001/038.jpg"
+    }
+];
+
+//console.log(img);
+
+localStorage.setItem("av6",JSON.stringify(img6));
+
+
+let arv6= JSON.parse(localStorage.getItem("av6"));
+
+
+arv6.map(function(ele){
+    let div6=document.createElement("div");
+
+    let pic6=document.createElement("img");
+
+    pic6.src=ele.image6;
+    div6.append(pic6);
+    document.querySelector("#arriveimg6").append(div6);
+})
+
+
+let img7=[
+    
+    {
+    image7:"https://assets.pbimgs.com/pbimgs/rk/images/dp/ecm/202141/1804/029/356.png"
+    },
+    {
+        image7:"https://assets.pbimgs.com/pbimgs/rk/images/dp/ecm/202141/1804/026/680.png"
+    },
+    {
+        image7:"https://assets.pbimgs.com/pbimgs/rk/images/dp/ecm/202141/1804/024/598.png"
+    },
+    {
+        image7:"https://assets.pbimgs.com/pbimgs/rk/images/dp/ecm/202141/1804/026/047.png"
+    },
+    {
+        image7:"https://assets.pbimgs.com/pbimgs/rk/images/dp/ecm/202141/1804/028/027.png"
+    }
+];
+
+//console.log(img);
+
+localStorage.setItem("av7",JSON.stringify(img7));
+
+
+let arv7= JSON.parse(localStorage.getItem("av7"));
+
+
+arv7.map(function(ele){
+    let div7=document.createElement("div");
+
+    let pic7=document.createElement("img");
+
+    pic7.src=ele.image7;
+    div7.append(pic7);
+    document.querySelector("#five").append(div7);
+})
+
+
+
+
+let img8=[
+    
+    {
+    image8:"https://assets.pbimgs.com/pbimgs/rk/images/dp/ecm/202210/1453/001/002.jpg"
+    },
+    {
+        image8:"https://assets.pbimgs.com/pbimgs/rk/images/dp/ecm/202210/1453/001/007.jpg"
+    },
+    {
+        image8:"https://assets.pbimgs.com/pbimgs/rk/images/dp/ecm/202210/1453/001/005.jpg"
+    },
+    {
+        image8:"https://assets.pbimgs.com/pbimgs/rk/images/dp/ecm/202210/1453/001/008.jpg"
+    },
+    {
+        image8:"https://assets.pbimgs.com/pbimgs/rk/images/dp/ecm/202210/1453/001/004.jpg"
+    },
+    {
+        image8:"https://assets.pbimgs.com/pbimgs/rk/images/dp/ecm/202210/1453/001/006.jpg"
+    },
+    {
+        image8:"https://assets.pbimgs.com/pbimgs/rk/images/dp/ecm/202210/1453/001/003.jpg"
+    },
+];
+
+//console.log(img);
+
+localStorage.setItem("av8",JSON.stringify(img8));
+
+
+let arv8= JSON.parse(localStorage.getItem("av8"));
+
+
+arv8.map(function(ele){
+    let div8=document.createElement("div");
+
+    let pic8=document.createElement("img");
+
+    pic8.src=ele.image8;
+    div8.append(pic8);
+    document.querySelector("#seven").append(div8);
+})
+
+
+
+
+
+
+
+
+
+let img9=[
+    
+    {
+    image9:"https://assets.pbimgs.com/pbimgs/rk/images/dp/ecm/202210/1453/001/009.jpg"
+    },
+    {
+        image9:"https://assets.pbimgs.com/pbimgs/rk/images/dp/ecm/202210/1453/001/010.jpg"
+    },
+  
+];
+
+//console.log(img);
+
+localStorage.setItem("av9",JSON.stringify(img9));
+
+
+let arv9= JSON.parse(localStorage.getItem("av9"));
+
+
+arv9.map(function(ele){
+    let div9=document.createElement("div");
+
+    let pic9=document.createElement("img");
+
+    pic9.src=ele.image9;
+    div9.append(pic9);
+    document.querySelector("#two").append(div9);
+})
