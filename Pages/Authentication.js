7db0ec597493093bf03d48cb6756ec91574273f7
HarshVardhan Dev <harshvardhansingh.1109@gmail.com>
2022-04-02 16:03:45 +0530

    Authentication pages

---
+++ Pages/Authentication.js
@@ -0,0 +1,17 @@
+
+
+// Following are the things that we have to made to make them work
+
+
+document.getElementById('sign-in').addEventListener('click', function () {
+
+  console.log("Sign in Process");
+}
+)
+
+
+document.getElementById('sign-up').addEventListener('click', function () {
+
+  console.log("Sign up Process");
+}
+)
